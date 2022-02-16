39a51a7eccbb5d9bb6c2bb02f7d4d3b671370af5
Yang-woo <ddonhlyw@naver.com>
2022-02-16 21:16:14 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    function getLocation() {
+      if (navigator.geolocation) {
+        // GPS를 지원하면
+        navigator.geolocation.getCurrentPosition(
+          function (position) {
+            alert(position.coords.latitude + " " + position.coords.longitude);
+          },
+          function (error) {
+            console.error(error);
+          },
+          {
+            enableHighAccuracy: false,
+            maximumAge: 0,
+            timeout: Infinity,
+          }
+        );
+      } else {
+        alert("GPS를 지원하지 않습니다");
+      }
+    }
+    getLocation();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +40,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          지도가 나오나?
         </a>
       </header>
     </div>
